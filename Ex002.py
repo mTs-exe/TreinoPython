f2e09a34e2736301bb0003213929d19b5509d781
Matheus de Oliveira Martins <122409193+mTs-exe@users.noreply.github.com>
2024-06-18 19:54:41 -0300

    Delete Ex002.py

--- Ex002.py
+++
@@ -1,5 +0,0 @@
-nome = input('Qual o seu nome? ')
-print('Seja muito bem vindo', nome, 'é um prazer te conhecer')
-
-nome = input('Qual o seu nome? ')
-print('Seja muito bem vindo {}!'.format(nome),'É um prazer te conhecer')
